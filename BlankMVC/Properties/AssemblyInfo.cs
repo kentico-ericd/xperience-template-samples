bdef52e6496b8f028c5944c150f6bd91454b2c92
Eric Dugre <ericd@kentico.com>
2020-12-26 14:45:56 -0500

    Init blank projects

---
+++ BlankMVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlankMVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("01556652-7c82-4492-9ad6-bc8a85b0a84d")]
+
+[assembly: AssemblyProduct("BlankMVC")]
+[assembly: AssemblyCopyright("© 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("13.0.0.0")]
+[assembly: AssemblyFileVersion("13.0.0.21407")]
+[assembly: AssemblyInformationalVersion("13.0.0")]
